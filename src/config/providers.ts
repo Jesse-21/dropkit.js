5c0281d4dc5f24caa78a771d6feefa0e02ded02a
Jesse21 <98306896+Jesse-21@users.noreply.github.com>
2022-03-30 10:54:44 -0400

    reverted to original until fix

--- src/config/providers.ts
+++ src/config/providers.ts
@@ -1,23 +1,22 @@
-import Torus from '@toruslabs/torus-embed'
-import WalletConnectProvider from '@walletconnect/web3-provider'
-import WalletLink from 'walletlink'
+// import Torus from '@toruslabs/torus-embed'
+// import WalletConnectProvider from '@walletconnect/web3-provider'
+// import WalletLink from 'walletlink'
 import { IProviderOptions } from 'web3modal'
 
 export const PROVIDER_OPTIONS: IProviderOptions = {
-walletconnect: {
- package: WalletConnectProvider,
-  options: {
-   infuraId: process.env.INFURA_ID,
-    },
-  },
-  torus: {
-    package: Torus,
-   },
-   walletlink: {
-    package: WalletLink,
-     options: {
-      infuraId: process.env.INFURA_ID,
-     },
-   },
+  // walletconnect: {
+  //   package: WalletConnectProvider,
+  //   options: {
+  //     infuraId: process.env.INFURA_ID,
+  //   },
+  // },
+  // torus: {
+  //   package: Torus,
+  // },
+  // walletlink: {
+  //   package: WalletLink,
+  //   options: {
+  //     infuraId: process.env.INFURA_ID,
+  //   },
+  // },
 }
-const dropkit = await DropKit.create('LCy1Nm6x8bQT6LEl2sjk', false, providers);
